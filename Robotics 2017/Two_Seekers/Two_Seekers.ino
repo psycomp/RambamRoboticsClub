a415fe903da41a7bddd27f443075a1f842aa7811
Steven Weinberger <steven@psycomp.com>
2017-04-25 19:58:24 -0400

    lego

---
+++ Robotics 2017/Two_Seekers/Two_Seekers.ino
@@ -0,0 +1,76 @@
+#include <Wire.h>
+#include <HTInfraredSeeker.h>
+
+extern "C" { 
+#include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
+
+#define MULTIPLEXER 0x70
+
+boolean SensorOne = false;
+boolean SensorTwo = false;
+ 
+void selectSeeker(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(MULTIPLEXER);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+void setup() {
+  while (!Serial);
+  delay(500);
+  Serial.begin(9600);
+  Wire.begin();
+
+  uint8_t data;
+  selectSeeker(0);
+  if (!twi_writeTo(MULTIPLEXER, &data, 0, 1, 1)) {
+    SensorOne = true;
+    InfraredSeeker::Initialize();
+  }
+  selectSeeker(1);
+  if (!twi_writeTo(MULTIPLEXER, &data, 0, 1, 1)) {
+    SensorTwo = true;
+    InfraredSeeker::Initialize();
+  }
+  delay(500);
+}
+
+void loop() {
+  int reading1, strength1;
+  int reading2, strength2;
+  InfraredResult InfraredBall1, InfraredBall2;
+
+  if(SensorOne) {
+    selectSeeker(0);
+    InfraredBall1 = InfraredSeeker::ReadAC();
+    reading1 = InfraredBall1.Direction - 5;
+    strength1 = InfraredBall1.Strength / 8;
+    if(reading1 == -5) {
+      reading1 = 0;
+      strength1 = 0;
+    }
+  }
+
+  if(SensorTwo) {
+    selectSeeker(1);
+    InfraredBall2 = InfraredSeeker::ReadAC();
+    reading2 = InfraredBall2.Direction - 5;
+    strength2 = InfraredBall2.Strength / 8;
+    if(reading2 == -5) {
+      reading2 = 0;
+      strength2 = 0;
+    }
+  }
+  
+  char buffer[100];
+  if(SensorOne && !SensorTwo)
+    sprintf(buffer,"%i,%i\n",reading1, strength1);
+  if(!SensorOne && SensorTwo)
+    sprintf(buffer,"%i,%i\n",reading2, strength2);
+  if(SensorOne && SensorTwo)
+    sprintf(buffer,"%i,%i,%i,%i\n",reading1, strength1, reading2, strength2);
+  Serial.print(buffer);
+}
+
